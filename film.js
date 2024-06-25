2de138abdc7366e75ee070a5f8d147ca7d9a2aaf
Grace <ggdwyer@travelers.com>
2024-06-25 15:16:11 -0400

    fixed planet issue

--- film.js
+++ film.js
@@ -2,7 +2,7 @@ const baseUrl = `https://swapi2.azurewebsites.net/api`;
 
 addEventListener('DOMContentLoaded', () => {
   nameH1 = document.querySelector('h1#name');
-  homeworldSpan = document.querySelector('span#homeworld');
+  planetUl = document.querySelector('#planets>ul');
   charactersUl = document.querySelector('#characters>ul');
   const sp = new URLSearchParams(window.location.search)
   const id = sp.get('id')
@@ -30,16 +30,10 @@ async function getFilms(id) {
 
   async function fetchHomeworld(film) {
     const url = `${baseUrl}/films/${film?.id}/planets`;
-  
-    try {
-      let planets = await fetch(url)
-        .then(res => res.json());
+    const planets = await fetch(url)
+        .then(res => res.json())
       return planets;
-    } catch (error) {
-      console.error('Error fetching homeworld.', error);
-      planets = "No planets found."
-      return planets;
-    }
+
   }
   
   async function fetchCharacter(film) {
@@ -52,7 +46,8 @@ async function getFilms(id) {
   const renderFilm = film => {
     // document.title = `SWAPI - ${character?.name}`;  // Just to make the browser tab say their name
     nameH1.textContent = film?.title;
-    homeworldSpan.innerHTML = `<a href="/planet.html?id=${film?.homeworld.id}">${film?.homeworld.name}</a>`;
+    const planetLis = film?.planets?.map(planet => `<li><a href="/planet.html?id=${planet.id}">${planet.name}</li>`)
+    planetUl.innerHTML = planetLis.join("");
     const characterLis = film?.characters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
     charactersUl.innerHTML = characterLis.join("");
   }
